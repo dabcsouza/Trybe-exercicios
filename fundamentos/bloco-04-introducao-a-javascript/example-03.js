d9da2ecc36b27a1fd3b1e721b9a3120846b21711
David Souza <dabcs@poli.ufrj.br>
2021-07-28 18:09:06 -0300

    realizacao do exercicio 3

---
+++ fundamentos/bloco-04-introducao-a-javascript/example-03.js
@@ -0,0 +1,9 @@
+nota = Math.floor(Math.random()*100 + 1);
+
+if (nota > 80) {
+    console.log("Parabéns, você foi aprovada(o)!");
+} else if (nota >= 60 && nota <=80 ) {
+    console.log("Você está na nossa lista de espera");
+}if (nota < 60) {
+    console.log("Você foi reprovada(o)");
+}
