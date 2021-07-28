8d388a5da340871d17d1cf5dc002765c442eeb27
David Souza <dabcs@poli.ufrj.br>
2021-07-28 15:54:59 -0300

    Criacao do arquivo README.md e meu primeiro arquivo java script

---
+++ fundamentos/bloco-04-introducao-a-javascript/first.js
@@ -0,0 +1,15 @@
+const myName = "David";
+const birthCity = "Santa Rita de Caldas";
+let birthYear = 1990;
+
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
+
+
+birthYear = 2030;
+
+console.log(birthYear);
+
+birthCity = "Ipuiuna";
+console.log(birthCity);
