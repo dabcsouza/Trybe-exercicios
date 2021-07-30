4bd2f19dc9e2e44bb98878d52e28e0ff8e90a282
David Souza <dabcs@poli.ufrj.br>
2021-07-30 18:45:55 -0300

    resolucao do exercicio 3

--- fundamentos/bloco-04-introducao-a-javascript/dia-03-logica-de-programacao-e-algoritmo/exercicios/ex2.js
+++ fundamentos/bloco-04-introducao-a-javascript/dia-03-logica-de-programacao-e-algoritmo/exercicios/ex2.js
@@ -1,8 +1,14 @@
 let n = 5;
-let ast = "";
+let esp = "";
 
 for (let i = 0; i < n; i += 1){
-    ast += '*';
-    console.log(ast);
+    for (let j = n; j > 0 ; j -= 1){
+        if (j > (i+1)) {
+            esp +=' ';
+        } else{
+            esp += '*';
+        }
+    }
+    console.log(esp);
+    esp="";
 }
-
---
+++ fundamentos/bloco-04-introducao-a-javascript/dia-03-logica-de-programacao-e-algoritmo/exercicios/ex3.js
@@ -0,0 +1,8 @@
+let n = 5;
+let ast = "";
+let esp = "";
+
+for (let i = 0; i < n; i += 1){
+    ast += '*';
+    console.log(ast);
+}
