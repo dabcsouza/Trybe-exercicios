96aa4fd9275efd7e856929676ba5c2f76d53a5e8
David Souza <dabcs@poli.ufrj.br>
2021-08-02 19:14:17 -0300

    execucao do exercicio 1 de for/in

--- fundamentos/bloco-04-introducao-a-javascript/dia-03-logica-de-programacao-e-algoritmo/exercicios/ex4.js
+++ fundamentos/bloco-04-introducao-a-javascript/dia-03-logica-de-programacao-e-algoritmo/exercicios/ex4.js
@@ -1,14 +1,19 @@
 let n = 5;
-let esp = "";
+let mid = n/2;
+let print = "";
 
 for (let i = 0; i < n; i += 1){
-    for (let j = n; j > 0 ; j -= 1){
-        if ((i%2)=== 1) {
-            esp +=' ';
+    for (let j = n - 1; j >= 0 ; j -= 1){
+        if ((i%2) === 1) {
+            print +=' ';
         } else{
-            if (j<);
+            if (j < mid - i/2 || j > mid + i) {
+                print +=' ';
+            } else {
+                print += '*';
+            }
         }
     }
-    console.log(esp);
-    esp="";
+    console.log(print);
+    print="";
 }
