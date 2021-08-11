7256a90760fda1dc0efe9e7c28e77223f0fe389e
David Souza <dabcs@poli.ufrj.br>
2021-08-11 16:45:29 -0300

    realizacao do exemplo 1 evento

--- fundamentos/bloco-05-JavaScript-DOM-Eventos-e-Web-Sorage/dia-03-Eventos/example/main.js
+++ fundamentos/bloco-05-JavaScript-DOM-Eventos-e-Web-Sorage/dia-03-Eventos/example/main.js
@@ -1,15 +1,34 @@
-const firstLi = document.getElementById('first-li');
-const secondLi = document.getElementById('second-li');
-const thirdLi = document.getElementById('third-li');
+const firstLi = document.getElementById('firstLi');
+const secondLi = document.getElementById('secondLi');
+const thirdLi = document.getElementById('thirdLi');
 const input = document.getElementById('input');
-const myWebpage = document.getElementById('my-spotrybefy');
+const myWebpage = document.getElementById('mySpotrybefy');
 
-/*
- Copie esse arquivo e edite apenas ele;
- Crie uma função que adicione a classe 'tech' ao elemento selecionado;
-1. Deve existir apenas um elemento com a classe 'tech'. Como você faz isso?
- Crie uma função que, ao digitar na caixa de texto, altere o texto do elemento
-com a classe 'tech';
+firstLi.addEventListener('click', changeClass);
+input.addEventListener('keyup', alteraNome);
+thirdLi.addEventListener('dblclick',redirect);
+
+
+function changeClass(element){
+  if (document.querySelector('.tech')){
+
+  } else {
+    element.target.className = "tech";
+  }
+}
+
+function alteraNome(){
+  document.querySelector('.tech').innerText = input.value;
+}
+
+function redirect(){
+  open('https://dabcsouza.github.io/');
+}
+
+
+
+/* 
+ 
  Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele
 redirecione para alguma página;
 1. Que tal redirecionar para seu portifólio?
@@ -18,16 +37,3 @@ a cor do mesmo;
 
 Segue abaixo um exemplo do uso de event.target:
 */
-
-function resetText(event) {
-  // O Event é passado como um parâmetro para a função.
-  event.target.innerText = 'Opção reiniciada';
-  // O event possui várias propriedades, porém a mais usada é o event.target,
-  // que retorna o objeto que disparou o evento.
-}
-
-firstLi.addEventListener('dblclick', resetText);
-
-// Não precisa passar o parâmetro dentro da callback resetText. O próprio
-// navegador fará esse trabalho por você, não é legal? Desse jeito, o
-// event.target na nossa função retornará o objeto 'firstLi'.
