222d8aa6351ac6c5a54a17a158ab3af5b198ccc4
David Souza <dabcs@poli.ufrj.br>
2021-08-11 20:29:57 -0300

    resolucao do exercicio 3

--- fundamentos/bloco-05-JavaScript-DOM-Eventos-e-Web-Sorage/dia-03-Eventos/exercises/script.js
+++ fundamentos/bloco-05-JavaScript-DOM-Eventos-e-Web-Sorage/dia-03-Eventos/exercises/script.js
@@ -1,16 +1,3 @@
-function createDaysOfTheWeek() {
-  const weekDays = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado']
-  const weekDaysList = document.querySelector('.week-days');
-
-  for (let index = 0; index < weekDays.length; index += 1) {
-    const days = weekDays[index];
-    const dayListItem = document.createElement('li');
-    dayListItem.innerHTML = days;
-
-    weekDaysList.appendChild(dayListItem);
-  };
-};
-
 createDaysOfTheWeek();
 
 const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
@@ -31,13 +18,44 @@ for (let i = 0; i < dezDaysList.length; i += 1){
   document.querySelector("#days").appendChild(liDays);
 }
 
-function createButton(sParameter){
 
+
+
+
+createButton('Feriados');
+document.querySelector('.btn-holiday').addEventListener("click",revealHoliday);
+
+
+
+function revealHoliday(){
+  let dayHoliday = document.getElementsByClassName("holiday");
+
+  for (let i = 0; i < dayHoliday.length; i += 1) {
+
+    if (dayHoliday[i].style.backgroundColor == '' || dayHoliday[i].style.backgroundColor == "rgb(238, 238, 238)") {
+    dayHoliday[i].style.backgroundColor = 'darkblue';
+    } else {dayHoliday[i].style.backgroundColor = "rgb(238, 238, 238)";}
+
+  }
+}
+
+
+function createButton(sParameter){
   button = document.createElement('button');
   button.className = "btn-holiday";
   button.innerText = sParameter;
   document.querySelector(".buttons-container").appendChild(button);
-
 }
 
-createButton('Feriados');
+function createDaysOfTheWeek() {
+  const weekDays = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado']
+  const weekDaysList = document.querySelector('.week-days');
+
+  for (let index = 0; index < weekDays.length; index += 1) {
+    const days = weekDays[index];
+    const dayListItem = document.createElement('li');
+    dayListItem.innerHTML = days;
+
+    weekDaysList.appendChild(dayListItem);
+  };
+};
