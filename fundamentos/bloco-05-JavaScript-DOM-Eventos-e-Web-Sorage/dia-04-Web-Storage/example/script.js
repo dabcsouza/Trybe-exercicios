3eb2dd5960d586edf37988e31cc14beea6601afd
David Souza <dabcs@poli.ufrj.br>
2021-08-18 14:45:19 -0300

    execucao do exemplo de aula

--- fundamentos/bloco-05-JavaScript-DOM-Eventos-e-Web-Sorage/dia-04-Web-Storage/example/script.js
+++ fundamentos/bloco-05-JavaScript-DOM-Eventos-e-Web-Sorage/dia-04-Web-Storage/example/script.js
@@ -2,16 +2,19 @@ const button = document.getElementById('add-button');
 const input = document.getElementById('phrases-input');
 const list = document.getElementById('phrases-list');
 
-function addPhraseToLocalStorage() {
-  const oldList = JSON.parse(localStorage.getItem('phrases'));
+function addPhraseToSessionStorage() {
+  if (sessionStorage.getItem('phrases') === null) {
+    sessionStorage.setItem('phrases', JSON.stringify([]));
+  }
+  const oldList = JSON.parse(sessionStorage.getItem('phrases'));
   const phraseText = input.value;
   oldList.push(phraseText);
-  localStorage.setItem('phrases', JSON.stringify(oldList));
+  sessionStorage.setItem('phrases', JSON.stringify(oldList));
   insertPhraseInDOM();
 };
 
 function insertPhraseInDOM() {
-  const phrasesList = JSON.parse(localStorage.getItem('phrases'));
+  const phrasesList = JSON.parse(sessionStorage.getItem('phrases'));
   const listLength = phrasesList.length - 1;
   const phraseText = phrasesList[listLength];
   const phrase = document.createElement('li');
@@ -19,22 +22,5 @@ function insertPhraseInDOM() {
   list.appendChild(phrase);
 };
 
-function initialRenderization() {
-  if (localStorage.getItem('phrases') === null) {
-    localStorage.setItem('phrases', JSON.stringify([]));
-  } else {
-    const phrasesList = JSON.parse(localStorage.getItem('phrases'));
-    const listLength = phrasesList.length - 1;
-    for (let index = 0; index <= listLength; index += 1) {
-      const listElement = document.createElement('li');
-      listElement.innerText = phrasesList[index];
-      list.appendChild(listElement);
-    };
-  };
-};
+button.addEventListener('click', addPhraseToSessionStorage);
 
-button.addEventListener('click', addPhraseToLocalStorage);
-
-window.onload = function() {
-  initialRenderization();
-};
