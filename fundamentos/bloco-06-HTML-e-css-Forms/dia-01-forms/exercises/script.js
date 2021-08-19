49542f9cb399ca7b1dcf02551477f6d8845b10b0
David Souza <dabcs@poli.ufrj.br>
2021-08-19 13:08:53 -0300

    continuacao do exemplo forms

--- fundamentos/bloco-06-HTML-e-css-Forms/dia-01-forms/exercises/script.js
+++ fundamentos/bloco-06-HTML-e-css-Forms/dia-01-forms/exercises/script.js
@@ -1,6 +1,8 @@
 const  selectEstate = document.getElementById('add-state');
 const estadosSigla = ["AC", "AL", "AP", "AM", "BA", "CE", "DF", "ES", "GO", "MA", "MT", "MS", "MG", "PA", "PB", "PR", "PE", "PI", "RJ", "RN", "RS", "RO", "RR", "SC", "SP", "SE", "TO"];
 const estadosNomes = ["Acre", "Alagoas", "Amapá", "Amazonas", "Bahia", "Ceará", "Distrito Federal", "Espírito Santo", "Goías", "Maranhão", "Mato Grosso", "Mato Grosso do Sul", "Minas Gerais", "Pará", "Paraíba", "Paraná", "Pernambuco", "Piauí", "Rio de Janeiro", "Rio Grande do Norte", "Rio Grande do Sul", "Rondônia", "Roraíma", "Santa Catarina", "São Paulo", "Sergipe", "Tocantins"];
+const document.getElementById('initialDate');
+
 
 for (let i = 0; i < estadosNomes.length; i += 1) {
     const option = document.createElement('option');
