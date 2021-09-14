6051de596de79db7e84650e8bf1057ab10467ed7
David Souza <dabcs@poli.ufrj.br>
2021-09-14 16:20:28 -0300

    teste do setTimeout

--- fundamentos/bloco-09-javaScript-assincrono-e-promises/dia-01-op-assincronas/example/ex1.js
+++ fundamentos/bloco-09-javaScript-assincrono-e-promises/dia-01-op-assincronas/example/ex1.js
@@ -4,16 +4,10 @@ const pushNumber = (list, number) => list.push(number);
 
 let numbers = [];
 
-pushNumber(numbers, 1);
-pushNumber(numbers, 2);
-pushNumber(numbers, 3);
-
-// assert.deepStrictEqual(numbers, [1, 2, 3]); // Passa
-
-
 setTimeout(() => pushNumber(numbers, 1), 3000);
 pushNumber(numbers, 2);
 pushNumber(numbers, 3);
-console.log
+console.table(numbers);
+assert.deepStrictEqual(numbers, [1, 2, 3]);
 
 
