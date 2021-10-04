f5ef99ed50f8e43b7bebf027c3ea03f3e89159de
David Souza <dabcs@poli.ufrj.br>
2021-10-04 18:36:45 -0300

    exercicio 1

--- front-end/bloco-11-Introdução-ao-React/dia-01-Hello-World-on-React/examples/my_example/src/App.js
+++ front-end/bloco-11-Introdução-ao-React/dia-01-Hello-World-on-React/examples/my_example/src/App.js
@@ -1,7 +1,11 @@
+import React from 'react';
 import './App.css';
+import MyComponent from './Component';
 
 function App() {
-  return();
+  return(
+    <MyComponent />
+  );
 }
 
 export default App;
---
+++ front-end/bloco-11-Introdução-ao-React/dia-01-Hello-World-on-React/examples/my_example/src/Component.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+class MyComponent extends Component {
+  render() {
+    return (
+    <div className = "about">
+    <h1> David Souza</h1>
+    <p> 
+      Tenho 31 anos, sou Engenheiro Químico e estudante de desenvolvimento FullStack na Trybe. 
+    </p>
+    </div>
+    )
+  }
+}
+
+export default MyComponent
