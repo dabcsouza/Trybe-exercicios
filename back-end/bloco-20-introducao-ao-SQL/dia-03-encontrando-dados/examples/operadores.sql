598542f0934301e5aa84a555defa304ce67ce184
David Souza <dabcs@poli.ufrj.br>
2022-01-26 18:53:28 -0300

    conclusão do exercício

--- back-end/bloco-20-introducao-ao-SQL/dia-03-encontrando-dados/examples/operadores.sql
+++ back-end/bloco-20-introducao-ao-SQL/dia-03-encontrando-dados/examples/operadores.sql
@@ -1,4 +1,7 @@
 USE sakila;
+
+-- Usando WHERE
+
 SELECT * FROM customer
 WHERE
 	email = 'LEONARD.SCHOFIELD@sakilacustomer.org';
@@ -9,3 +12,69 @@ SELECT * FROM customer
     AND first_name <> 'KENNETH'
     ORDER BY first_name ASC;
     
+SELECT
+	title AS 'título',
+    description AS 'Descrição',
+    release_year AS 'Ano de Lançamento',
+    replacement_cost AS 'custo de substituição'
+FROM film
+WHERE
+	rating <> 'NC-17' AND
+    replacement_cost >= 18.00
+ORDER BY replacement_cost DESC, title ASC
+LIMIT 100;
+
+SELECT count(active) AS 'Clientes Ativos' FROM customer 
+WHERE active = TRUE
+GROUP BY active;
+
+SELECT * FROM customer
+WHERE NOT active;
+
+SELECT * FROM film
+WHERE 
+	rating = 'NC-17'
+ORDER BY
+	rental_rate ASC,
+    title ASC
+LIMIT 50;
+
+-- Usando LIKE 
+
+SELECT * FROM film
+	WHERE title LIKE '%ace%';
+
+SELECT * FROM film
+	WHERE description LIKE '%china';
+
+SELECT * FROM film
+	WHERE description LIKE '%girl%'
+    AND title LIKE '%lord';
+
+SELECT * FROM film
+	WHERE description LIKE '%Documentary%'
+    AND title LIKE '___gon%';
+
+SELECT * FROM film
+	WHERE title LIKE '%academy'
+    OR title LIKE 'mosquito%'; 
+
+SELECT COUNT(*) FROM film
+	WHERE description LIKE '%monkey%'
+    AND description LIKE '%sumo%';
+
+
+
+SELECT title, rental_duration, rating FROM film
+WHERE rating IN('R', 'PG-13')
+AND rental_duration = 6;
+
+SELECT * FROM film
+WHERE title 
+IN('AFFAIR PREJUDICE', 'AFRICAN EGG', 'AIRPLANE SIERRA');
+
+USE sakila;
+SELECT * FROM actor
+WHERE first_name BETWEEN 'B' AND 'E%'
+ORDER BY first_name;
+
---
+++ back-end/bloco-20-introducao-ao-SQL/dia-03-encontrando-dados/exercicises/exercise.sql
@@ -0,0 +1,33 @@
+USE PecasFornecedores;
+SELECT * FROM Pecas
+WHERE
+	name LIKE 'GR%';
+
+SELECT * FROM Fornecimentos
+WHERE
+	peca = 2
+ORDER BY Fornecedor;
+
+SELECT 
+	peca AS 'Peça',
+    preco AS 'Preço',
+    Fornecedor FROM Fornecimentos
+WHERE
+	Fornecedor LIKE '%N%';
+    
+SELECT * FROM Fornecedores
+WHERE
+	name LIKE '%LTDA%'
+ORDER BY name DESC;
+
+SELECT COUNT(*) FROM Fornecedores
+WHERE code LIKE '%F%';
+
+SELECT * FROM Fornecimentos
+WHERE Preco BETWEEN '15' AND '40'
+ORDER BY Preco;
+
+SELECT 
+	SUM(quantity) AS 'Quantidade de vendas'
+    FROM PecasFornecedores.Vendas
+WHERE order_date BETWEEN '2018-04-15' AND '2019-07-30';
